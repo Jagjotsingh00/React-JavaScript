73fc7675ac897a7d12981c0340254086f12a6ec2
Jagjotsingh00 <iamjagjot02@gmail.com>
2026-03-10 12:12:26 +0530

    Render and root practice

--- app.js
+++ app.js
@@ -1,18 +1,35 @@
+// with JavaScript 
 const roots = document.getElementById("root");
 const heading = document.createElement("heading");
 heading.innerText = "Hello this is this JavaScript DOM";
 roots.appendChild(heading);
 
+// // ---------------------------------------------------------------------------
+
+// // with React
+
+// const parent = React.createElement(
+//     "div",
+//     {id:"parent"},
+//     React.createElement(
+//         "div",
+//         {id: "child"},
+//         React.createElement("h1",{},"This is h1 tag in React")
+//     )
+// )
+// const root = ReactDOM.createRoot(document.getElementById("root"))
+// root.render(parent);
 
 
-const parent = React.createElement(
-    "div",
-    {id:"parent"},
-    React.createElement(
-        "div",
-        {id: "child"},
-        React.createElement("h1",{},"This is h1 tag in React")
-    )
+
+// -------------------------------------------------------------------------------
+
+const fam = React.createElement(
+    "div", 
+    {id: "father"},
+    React.createElement("h1",{},"I am father"),
+    React.createElement("h1",{},"I am mother"),
+    React.createElement("h1",{},"I am child")
 )
-const root = ReactDOM.createRoot(document.getElementById("root"))
-root.render(parent);
+const member = ReactDOM.createRoot(document.getElementById("home"))
+member.render(fam);
